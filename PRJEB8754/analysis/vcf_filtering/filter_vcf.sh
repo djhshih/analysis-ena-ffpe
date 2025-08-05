8355b0d2cc8064d1b08e4986d7b1ba505aa8f021
MoyukhShabon <moyukh.work@gmail.com>
2025-08-05 18:19:31 +0800

    added vcf filtereing script based on updated criteria

--- PRJEB8754/analysis/vcf_filtering/filter_vcf.sh
+++ PRJEB8754/analysis/vcf_filtering/filter_vcf.sh
@@ -1,19 +1,30 @@
-#!/bin/env/bash
+#!/bin/bash
 
 set -euo pipefail
 
-vcf_dir="../../data/vcf_filtermutectcalls"
-root_outdir="../../data/vcf_ad_filtered"
+root_outdir="../../data/vcf_pass-orient-pos-sb_ad_filtered"
+mkdir -p $root_outdir
 
-for vcf in ${vcf_dir}/*/*.vcf; do
+filter_expression='(FILTER="PASS" | FILTER="strand_bias" | FILTER="orientation" | FILTER="position" | FILTER="strand_bias;orientation" | FILTER="strand_bias;position" | FILTER="orientation;position" | FILTER="strand_bias;orientation;position") & (FMT/AD[0:0] + FMT/AD[0:1] >= 10 & FMT/AD[0:1] >=3)'
+echo -e "Filtering Expression: $filter_expression"
 
-    filename=$(basename $vcf)
-    sample_id=${filename%%.*}
+for vcf in ../../data/vcf_filtermutectcalls_obf/*/*.vcf; do
     
-    echo $sample_id
-    outdir=${root_outdir}/${sample_id}
+    filename=$(basename $vcf)
+    sample_name=${filename%%.*}
+
+    echo $sample_name
+
+    outdir="${root_outdir}/${sample_name}"
     mkdir -p $outdir
 
-    bcftools view -o ${outdir}/${sample_id}.vcf --include 'FMT/AD[0:0] + FMT/AD[0:1] >= 10 && FMT/AD[0:1] >= 3' $vcf
+    echo -e "\nFiltering $filename"
+    bcftools view -i "$filter_expression" $vcf -o "${outdir}/${sample_name}.vcf"
+    echo "Filtered VCF saved to ${outdir}/${sample_name}.vcf"
+
+    echo "Indexing ${outdir}/${sample_name}.vcf"
+    gatk IndexFeatureFile -I "${outdir}/${sample_name}.vcf"
 
 done
+
+echo -e "\nFinished."
