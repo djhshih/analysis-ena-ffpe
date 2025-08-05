cc77c48198e7895b6e33fb98c0249508632c53cd
MoyukhShabon <moyukh.work@gmail.com>
2025-08-05 17:40:49 +0800

    bug fix

--- SRP044740/analysis/vcf_alignment_artifact_filter/run_serial.sh
+++ SRP044740/analysis/vcf_alignment_artifact_filter/run_serial.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 ls ../../data/vcf_filtermutectcalls_obf/*/*.vcf > dlazy_samples.txt
 
-djobs dlazy_samples.txt filter_alignment_artifacts.sh
+djobs dlazy_samples.txt bash filter_alignment_artifacts.sh
 
 dlazy job
