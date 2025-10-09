21dacaae506e480a1ef78388b4a9311ef606f245
MoyukhShabon <moyukh.work@gmail.com>
2025-10-10 02:27:25 +0800

    updated microsec input creation script
    This now includes PRJEB44073 dataset

--- ffpe-snvf/ffpe_artifact_filtering/make-microsec-inputs.py
+++ ffpe-snvf/ffpe_artifact_filtering/make-microsec-inputs.py
@@ -1,3 +1,4 @@
+# %%
 #!/usr/bin/python3
 import polars as pl
 import pysam
@@ -7,10 +8,11 @@ import os
 import glob
 
 
+# %%
 
 ## Create Mutation Info for MicroSEC
 
-def import_formatted_vcf(vcf_path: str) -> pl.DataFrame:
+def import_formatted_vcf(vcf_path: str, ct_only: bool = False) -> pl.DataFrame:
 	"""
 	Reads a VCF file and returns a Polars DataFrame formatted for MicroSEC analysis.
 
@@ -56,15 +58,23 @@ def import_formatted_vcf(vcf_path: str) -> pl.DataFrame:
 		)
 		.explode("Alt")
 	)
+ 
+	if(ct_only):
+		vcf = vcf.filter(
+			((pl.col("Ref") == "C") & (pl.col("Alt") == "T")) |
+			((pl.col("Ref") == "G") & (pl.col("Alt") == "A"))
+		)
+
 	return vcf
 
 
-
-def make_mut_info(vcf_path: str, sample_name: str, genome: pysam.FastaFile) -> pl.DataFrame:
+# %%
+def make_mut_info(vcf_path: str, sample_name: str, genome: pysam.FastaFile, ct_only: bool = False) -> pl.DataFrame:
     
-	mut_info = import_formatted_vcf(vcf_path).with_columns(pl.lit(sample_name).alias("Sample"))
+	mut_info = import_formatted_vcf(vcf_path, ct_only).with_columns(pl.lit(sample_name).alias("Sample"))
 	
 	# Define a function to be applied to each row-like struct
+	# Used to retrieve neighbourhood sequence via map_elements which is vectorized for speed
 	def get_neighborhood_sequence(row_struct: dict) -> str:
 		chrom = row_struct["Chr"]
 		pos = row_struct["Pos"]
@@ -123,7 +133,10 @@ def make_mut_info(vcf_path: str, sample_name: str, genome: pysam.FastaFile) -> p
  
 	return mut_info
 
+# %%
 ## PRJEB8754
+
+print("Making inputs for ENA PRJEB8754")
 
 outdir_root = "../PRJEB8754/vcf_pass-orient-pos-sb_ad_filtered/microsec/inputs"
 genome = pysam.FastaFile("../../data/ref/Homo_sapiens_assembly38.fasta")
@@ -148,6 +161,7 @@ lookup_table = lookup_table.with_columns([
 
 lookup_table_ffpe = lookup_table.filter(pl.col("preservation") == "FFPE")
 
+# %%
 for i in range(lookup_table_ffpe.shape[0]):
 	alias = lookup_table_ffpe[i, "sample_alias"]
 	run_acc = lookup_table_ffpe[i, "run_accession"]
@@ -157,12 +171,13 @@ for i in range(lookup_table_ffpe.shape[0]):
 	outdir = f"{outdir_root}/mut_info"
 	os.makedirs(outdir, exist_ok=True)
 
-	mut_info = make_mut_info(lookup_table_ffpe[i, "vcf_path"], sample_name, genome)
+	mut_info = make_mut_info(lookup_table_ffpe[i, "vcf_path"], sample_name, genome, ct_only = True)
 
 	mut_info.write_csv(f"{outdir}/{sample_name}.microsec.mut-info.tsv", separator="\t")
 	print(f"Finished creating Mut-Info for - {i + 1}. {sample_name}")
 
 
+# %%
 ### Create Sample Info for MicroSEC
 
 mut_info_paths = sorted(glob.glob(f"{outdir_root}/mut_info/*.microsec.mut-info.tsv"))
@@ -193,7 +208,10 @@ for i in range(len(mut_info_paths)):
 sample_info.to_csv(f"{outdir_root}/microsec.sample_info.tsv", sep="\t", index=False, header=False)
 
 
+# %%
 ## SRP044740
+
+print("Making inputs for ENA SRP044740")
 
 outdir_root = "../SRP044740/vcf_filtered_pass_orientation/microsec/inputs"
 genome = pysam.FastaFile("../../data/ref/Homo_sapiens_assembly38.fasta")
@@ -217,8 +235,9 @@ lookup_table = lookup_table.with_columns([
 ])
 
 lookup_table_ffpe = lookup_table.filter(pl.col("sample_type") == "FFPE")
-lookup_table_ffpe
+# lookup_table_ffpe
 
+# %%
 for i in range(lookup_table_ffpe.shape[0]):
 	alias = lookup_table_ffpe[i, "sample_alias"]
 	run_acc = lookup_table_ffpe[i, "run_accession"]
@@ -228,11 +247,12 @@ for i in range(lookup_table_ffpe.shape[0]):
 	outdir = f"{outdir_root}/mut_info"
 	os.makedirs(outdir, exist_ok=True)
 
-	mut_info = make_mut_info(lookup_table_ffpe, i, sample_name, genome)
+	mut_info = make_mut_info(lookup_table_ffpe[i, "vcf_path"], sample_name, genome, ct_only = True)
 
 	mut_info.write_csv(f"{outdir}/{sample_name}.microsec.mut-info.tsv", separator="\t")
 	print(f"Finished creating Mut-Info for - {i + 1}. {sample_name}")
 
+# %%
 ### Create Sample Info for MicroSEC
 
 mut_info_paths = sorted(glob.glob(f"{outdir_root}/mut_info/*.microsec.mut-info.tsv"))
@@ -253,6 +273,150 @@ for i in range(len(mut_info_paths)):
 	sample_info.loc[i, "BAM file"] = lookup[0, "bam_path"]
 	
 	sample_info.loc[i, "read length"] = str(90) # majoriy read length in bam files
+	sample_info.loc[i, "adapter sequence read 1"] = np.nan
+	sample_info.loc[i, "optional: adapter sequence read 2"] = np.nan
+	sample_info.loc[i, "sample type: Human or Mouse"] = "hg38"
+	sample_info.loc[i, "panel name"] = "TOP"
+	sample_info.loc[i, "optional: reference genome fasta file"] = ref_path
+	#sample_info.loc[i, "optional: simple repeat region bed file"] = np.nan
+
+sample_info.to_csv(f"{outdir_root}/microsec.sample_info.tsv", sep="\t", index=False, header=False)
+
+
+# %%
+## SRP065941
+
+print("Making inputs for ENA SRP065941")
+
+outdir_root = "../SRP065941/vcf_filtered_pass_orientation/microsec/inputs"
+genome = pysam.FastaFile("../../data/ref/Homo_sapiens_assembly38.fasta")
+lookup_table = pl.read_csv("../../annot/SRP065941/sample-annotation_stage1.tsv", separator="\t").filter(pl.col("sample_type") == "Tumor")
+
+
+
+vcf_paths = sorted(glob.glob("../../vcf/SRP065941/vcf_filtered_pass_orientation/*/*.vcf"))
+bam_paths = sorted(glob.glob(f"../../data/SRP065941/bam/*/*.bam"))
+
+
+lookup_table = lookup_table.with_columns([
+	pl.col("sample_name").map_elements(
+		lambda id: [os.path.abspath(path) for path in vcf_paths if id in path][0],
+		return_dtype=pl.String
+	).alias("vcf_path"),
+  
+ 	pl.col("sample_name").map_elements(
+		lambda name : [os.path.abspath(path) for path in bam_paths if name in path][0],
+		return_dtype=pl.String
+	).alias("bam_path")
+])
+
+lookup_table_ffpe = lookup_table.filter(pl.col("preservation") == "FFPE")
+# lookup_table_ffpe
+
+# %%
+for i in range(lookup_table_ffpe.shape[0]):
+	sample_name = lookup_table_ffpe[i, "sample_name"]
+	
+	outdir = f"{outdir_root}/mut_info"
+	os.makedirs(outdir, exist_ok=True)
+
+	mut_info = make_mut_info(lookup_table_ffpe[i, "vcf_path"], sample_name, genome, ct_only = True)
+
+	mut_info.write_csv(f"{outdir}/{sample_name}.microsec.mut-info.tsv", separator="\t")
+	print(f"Finished creating Mut-Info for - {i + 1}. {sample_name}")
+
+# %%
+### Create Sample Info for MicroSEC
+
+mut_info_paths = sorted(glob.glob(f"{outdir_root}/mut_info/*.microsec.mut-info.tsv"))
+ref_path = os.path.abspath('../../data/ref/Homo_sapiens_assembly38.fasta')
+mut_info_suffix = ".microsec.mut-info.tsv"
+
+sample_info = pd.DataFrame()
+
+for i in range(len(mut_info_paths)):
+	
+	sample_name = os.path.basename(mut_info_paths[i]).split(mut_info_suffix)[0]
+	
+	lookup = lookup_table_ffpe.filter(pl.col("sample_name").str.contains(sample_name))
+
+	sample_info.loc[i, "sample_name"] = sample_name
+	sample_info.loc[i, "mutation information tsv file"] = os.path.abspath(mut_info_paths[i])
+	sample_info.loc[i, "BAM file"] = lookup[0, "bam_path"]
+	
+	sample_info.loc[i, "read length"] = str(100) # majoriy read length in bam files
+	sample_info.loc[i, "adapter sequence read 1"] = np.nan
+	sample_info.loc[i, "optional: adapter sequence read 2"] = np.nan
+	sample_info.loc[i, "sample type: Human or Mouse"] = "hg38"
+	sample_info.loc[i, "panel name"] = "TOP"
+	sample_info.loc[i, "optional: reference genome fasta file"] = ref_path
+	#sample_info.loc[i, "optional: simple repeat region bed file"] = np.nan
+
+sample_info.to_csv(f"{outdir_root}/microsec.sample_info.tsv", sep="\t", index=False, header=False)
+
+
+# %%
+## PRJEB44073
+
+print("Making inputs for ENA PRJEB44073")
+
+outdir_root = "../PRJEB44073/vcf_filtered_pass_orientation/microsec/inputs"
+genome = pysam.FastaFile("../../data/ref/Homo_sapiens_assembly38.fasta")
+lookup_table = pl.read_csv("../../annot/PRJEB44073/sample-info_stage2.tsv", separator="\t")
+
+
+vcf_paths = sorted(glob.glob("../../vcf/PRJEB44073/vcf_filtered_pass_orientation/*/*.vcf"))
+bam_paths = sorted(glob.glob(f"../../data/PRJEB44073/bam/*/*.bam"))
+
+# Skipping failed sample for now
+lookup_table = lookup_table.filter(pl.col("sample_alias") != "Sample_B83_0029")
+
+lookup_table = lookup_table.with_columns([
+	pl.col("sample_alias").map_elements(
+		lambda id: [os.path.abspath(path) for path in vcf_paths if id in path][0],
+		return_dtype=pl.String
+	).alias("vcf_path"),
+  
+ 	pl.col("sample_alias").map_elements(
+		lambda name : [os.path.abspath(path) for path in bam_paths if name in path][0],
+		return_dtype=pl.String
+	).alias("bam_path")
+])
+
+lookup_table_ffpe = lookup_table.filter(pl.col("preservation") == "FFPE")
+
+# %%
+for i in range(lookup_table_ffpe.shape[0]):
+	sample_name = lookup_table_ffpe[i, "sample_alias"]
+	
+	outdir = f"{outdir_root}/mut_info"
+	os.makedirs(outdir, exist_ok=True)
+
+	mut_info = make_mut_info(lookup_table_ffpe[i, "vcf_path"], sample_name, genome, ct_only = True)
+
+	mut_info.write_csv(f"{outdir}/{sample_name}.microsec.mut-info.tsv", separator="\t")
+	print(f"Finished creating Mut-Info for - {i + 1}. {sample_name}")
+
+# %%
+### Create Sample Info for MicroSEC
+
+mut_info_paths = sorted(glob.glob(f"{outdir_root}/mut_info/*.microsec.mut-info.tsv"))
+ref_path = os.path.abspath('../../data/ref/Homo_sapiens_assembly38.fasta')
+mut_info_suffix = ".microsec.mut-info.tsv"
+
+sample_info = pd.DataFrame()
+
+for i in range(len(mut_info_paths)):
+	
+	sample_name = os.path.basename(mut_info_paths[i]).split(mut_info_suffix)[0]
+	
+	lookup = lookup_table_ffpe.filter(pl.col("sample_alias").str.contains(sample_name))
+
+	sample_info.loc[i, "sample_name"] = sample_name
+	sample_info.loc[i, "mutation information tsv file"] = os.path.abspath(mut_info_paths[i])
+	sample_info.loc[i, "BAM file"] = lookup[0, "bam_path"]
+	
+	sample_info.loc[i, "read length"] = str(98) # majoriy read length in bam files
 	sample_info.loc[i, "adapter sequence read 1"] = np.nan
 	sample_info.loc[i, "optional: adapter sequence read 2"] = np.nan
 	sample_info.loc[i, "sample type: Human or Mouse"] = "hg38"
