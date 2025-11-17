b4ae2570fbf2c9ddbf2b48e5c427ab478a4dffd2
MoyukhShabon <moyukh.work@gmail.com>
2025-11-17 18:30:52 +0800

    updated ffpe-snvf script creation method
    - added ideafix script creation functionality
    
    - made inputs paths for positional arguments to be absolute

--- ffpe-snvf/ffpe_artifact_filtering/create_scripts.py
+++ ffpe-snvf/ffpe_artifact_filtering/create_scripts.py
@@ -1,115 +1,125 @@
 import os
 import glob
-from tkinter.ttk import Separator
+from weakref import ref
 import polars as pl
 
+## Functions
+def return_path_if_exists(path: str, abs: bool =False) -> str:
+	if path.endswith(".bam"):
+		return os.path.abspath(path) if abs else path
+	elif os.path.exists(path):
+		return os.path.abspath(path) if abs else path
+	else:
+		raise FileNotFoundError(f"Path {path} does not exist.")
+
+def create_filtering_scripts(models: list, vcf_path: str, bam_path: str, filtered_outdir: str, sample_name: str, ref_path: str) -> None:
+	for model in models:
+	
+		if model == "ideafix":
+			
+			outdir = "script_ideafix"
+			os.makedirs(outdir, exist_ok=True)
+					
+			## Template script for each model
+			template_dir = return_path_if_exists(f"{model}.R", abs=True)
+			content = f"#!/usr/bin/env Rscript\nRscript {template_dir} --vcf '{vcf_path}' --ref '{ref_path}' --outdir '{filtered_outdir}'\n"
+			filename = f"{outdir}/{model}_{sample_name}.sh"
+			
+			with open(filename, "w") as f:
+				f.write(content)
+				
+		else:
+			
+			outdir = "script"
+			os.makedirs(outdir, exist_ok=True)
+			
+			template_dir = return_path_if_exists(f"{model}.sh.template", abs=True)
+		
+			content = f"#!/bin/bash\nbash {template_dir} '{bam_path}' '{vcf_path}' '{filtered_outdir}' '{ref_path}'\n"
+			filename = f"{outdir}/{model}_{sample_name}.sh"
+		
+		with open(filename, "w") as f:
+			f.write(content)
+			
+		print(f"Created {model} filtering script for {vcf_path}")
+
+#--------------------
+
+## user parameters
+models = ["mobsnvf", "vafsnvf", "sobdetector", "ideafix"]
+ref_path = return_path_if_exists("../../data/ref/Homo_sapiens_assembly38.fasta", abs=True)
+
+
+# Process each dataset
+
 ## PRJEB8754
-vcf_paths = glob.glob("../../vcf/PRJEB8754/vcf_pass-orient-pos-sb_ad_filtered/*/*.vcf")
-filtered_outdir = "../../ffpe-snvf/PRJEB8754/vcf_pass-orient-pos-sb_ad_filtered"
+vcf_paths = sorted([os.path.abspath(path) for path in glob.glob("../../vcf/PRJEB8754/vcf_pass-orient-pos-sb_ad_filtered/*/*.vcf")])
+filtered_outdir = os.path.abspath("../PRJEB8754/vcf_pass-orient-pos-sb_ad_filtered")
 bam_dir = "../../data/PRJEB8754/bam"
 
-models = ["mobsnvf", "vafsnvf", "sobdetector"]
-
-outdir = "script"
-os.makedirs(outdir, exist_ok=True)
-
+# skip non FFPE vcfs
 for vcf_path in vcf_paths:
-    if "Frozen" in vcf_path:
-        continue
-    
-    vcf_filename = os.path.basename(vcf_path)
-    sample_name = vcf_filename.removesuffix(".vcf")
-    
-    for model in models:
-        content = f"#!/bin/bash\nbash ../{model}.sh.template '../../../data/bam/{sample_name}.bam' '../{vcf_path}' '../{filtered_outdir}'\n"
-        filename = f"{outdir}/{model}_{sample_name}.sh"
-        
-        with open(filename, "w") as f:
-            f.write(content)
-            
-        print(f"Created {model} filtering script for {vcf_path}")
+	if "Frozen" in vcf_path:
+		continue
+	
+	vcf_filename = os.path.basename(vcf_path)
+	sample_name = vcf_filename.removesuffix(".vcf")
+	
+	bam_path = return_path_if_exists(f"{bam_dir}/{sample_name}/{sample_name}.bam", abs=True)
+	create_filtering_scripts(models, vcf_path, bam_path, filtered_outdir, sample_name, ref_path)
 
 
 ## SRP044740
-vcf_paths = sorted(glob.glob("../../vcf/SRP044740/vcf_filtered_pass_orientation/*/*.vcf"))
-filtered_outdir = "../../ffpe-snvf/SRP044740/filtered_pass_orientation"
+vcf_paths = sorted([os.path.abspath(path) for path in glob.glob("../../vcf/SRP044740/vcf_filtered_pass_orientation/*/*.vcf")])
+filtered_outdir = os.path.abspath("../SRP044740/vcf_filtered_pass_orientation")
 bam_dir = "../../data/SRP044740/bam"
 
-models = ["mobsnvf", "vafsnvf", "sobdetector"]
-
-outdir = "script"
-os.makedirs(outdir, exist_ok=True)
-
+# skip non FFPE vcfs
 for vcf_path in vcf_paths:
-    if "FROZ" in vcf_path:
-        continue
-    
-    vcf_filename = os.path.basename(vcf_path)
-    sample_name = vcf_filename.removesuffix(".vcf")
-    
-    for model in models:
-        content = f"#!/bin/bash\nbash ../{model}.sh.template '../{bam_dir}/{sample_name}.bam' '../{vcf_path}' '../{filtered_outdir}'\n"
-        filename = f"{outdir}/{model}_{sample_name}.sh"
-        
-        with open(filename, "w") as f:
-            f.write(content)
-            
-        print(f"Created {model} filtering script for {vcf_path}")
+	if "FROZ" in vcf_path:
+		continue
+	
+	vcf_filename = os.path.basename(vcf_path)
+	sample_name = vcf_filename.removesuffix(".vcf")
+	
+	bam_path = return_path_if_exists(f"{bam_dir}/{sample_name}/{sample_name}.bam", abs=True)
+	create_filtering_scripts(models, vcf_path, bam_path, filtered_outdir, sample_name, ref_path)
 
 
 ## SRP065941
-vcf_paths = sorted(glob.glob("../../vcf/SRP065941/vcf_filtered_pass_orientation/*/*.vcf"))
-filtered_outdir = "../../ffpe-snvf/SRP065941/vcf_filtered_pass_orientation"
+vcf_paths = sorted([os.path.abspath(path) for path in glob.glob("../../vcf/SRP065941/vcf_filtered_pass_orientation/*/*.vcf")])
+filtered_outdir = os.path.abspath("../SRP065941/vcf_filtered_pass_orientation")
 bam_dir = "../../data/SRP065941/bam"
 
-models = ["mobsnvf", "vafsnvf", "sobdetector"]
-
-outdir = "script"
-os.makedirs(outdir, exist_ok=True)
-
+# skip non FFPE vcfs
 for vcf_path in vcf_paths:
-    if "frozen" in vcf_path:
-        continue
-    
-    vcf_filename = os.path.basename(vcf_path)
-    sample_name = vcf_filename.removesuffix(".vcf")
-    
-    for model in models:
-        content = f"#!/bin/bash\nbash ../{model}.sh.template '../{bam_dir}/{sample_name}/{sample_name}.bam' '../{vcf_path}' '../{filtered_outdir}'\n"
-        filename = f"{outdir}/{model}_{sample_name}.sh"
-        
-        with open(filename, "w") as f:
-            f.write(content)
-            
-        print(f"Created {model} filtering script for {vcf_path}")
-        
-        
+	if "frozen" in vcf_path:
+		continue
+	
+	vcf_filename = os.path.basename(vcf_path)
+	sample_name = vcf_filename.removesuffix(".vcf")
+	
+	bam_path = return_path_if_exists(f"{bam_dir}/{sample_name}/{sample_name}.bam", abs=True)
+	create_filtering_scripts(models, vcf_path, bam_path, filtered_outdir, sample_name, ref_path)
+
+
 ## PRJEB44073
-vcf_paths = sorted(glob.glob("../../vcf/PRJEB44073/vcf_filtered_pass_orientation/*/*.vcf"))
-filtered_outdir = "../../ffpe-snvf/PRJEB44073/vcf_filtered_pass_orientation"
+vcf_paths = sorted([os.path.abspath(path) for path in glob.glob("../../vcf/PRJEB44073/vcf_filtered_pass_orientation/*/*.vcf")])
+filtered_outdir = os.path.abspath("../PRJEB44073/vcf_filtered_pass_orientation")
 bam_dir = "../../data/PRJEB44073/bam"
 
 annot = pl.read_csv("../../annot/PRJEB44073/sample-info_stage2.tsv", separator = "\t")
 ffpe_samples = annot.filter(pl.col("preservation") == "FFPE").get_column("sample_alias").to_list()
 
+# skip non FFPE vcfs
 vcf_paths = [path for path in vcf_paths if any(sample in path for sample in ffpe_samples)]
 
-models = ["mobsnvf", "vafsnvf", "sobdetector"]
-
-outdir = "script"
-os.makedirs(outdir, exist_ok=True)
-
 for vcf_path in vcf_paths:
-    
-    vcf_filename = os.path.basename(vcf_path)
-    sample_name = vcf_filename.removesuffix(".vcf")
-    
-    for model in models:
-        content = f"#!/bin/bash\nbash ../{model}.sh.template '../{bam_dir}/{sample_name}/{sample_name}.bam' '../{vcf_path}' '../{filtered_outdir}'\n"
-        filename = f"{outdir}/{model}_{sample_name}.sh"
-        
-        with open(filename, "w") as f:
-            f.write(content)
-            
-        print(f"Created {model} filtering script for {vcf_path}")
-        
+	
+	vcf_filename = os.path.basename(vcf_path)
+	sample_name = vcf_filename.removesuffix(".vcf")
+	
+	bam_path = return_path_if_exists(f"{bam_dir}/{sample_name}/{sample_name}.bam", abs=True)
+	create_filtering_scripts(models, vcf_path, bam_path, filtered_outdir, sample_name, ref_path)
+	
+print("All filtering scripts created.")
