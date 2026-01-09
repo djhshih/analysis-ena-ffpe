ea93f736c8020f97b58e7eebf7063d07dc6d1dcf
MoyukhShabon <moyukh.work@gmail.com>
2026-01-09 18:41:18 +0800

    updated vcf filter script

--- vcf/PRJEB8754/filter_vcf.sh
+++ vcf/PRJEB8754/filter_vcf.sh
@@ -2,13 +2,13 @@
 
 set -euo pipefail
 
-root_outdir="vcf_pass-orient-pos-sb_ad_filtered"
+root_outdir="vcf_filtered_pass-orient-pos-sb-ad"
 mkdir -p $root_outdir
 
 filter_expression='(FILTER="PASS" | FILTER="strand_bias" | FILTER="orientation" | FILTER="position" | FILTER="strand_bias;orientation" | FILTER="strand_bias;position" | FILTER="orientation;position" | FILTER="strand_bias;orientation;position") & (FMT/AD[0:0] + FMT/AD[0:1] >= 10 & FMT/AD[0:1] >=3)'
 echo -e "Filtering Expression: $filter_expression"
 
-for vcf in vcf_filtermutectcalls_obmm_unfiltered/*/*.vcf; do
+for vcf in vcf_filtermutectcalls_obmm_unfiltered_default-annot/*/*.vcf; do
     
     filename=$(basename $vcf)
     sample_name=${filename%%.*}
