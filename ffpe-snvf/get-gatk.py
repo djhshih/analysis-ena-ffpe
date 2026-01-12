26329cbb6526481de18d4214b2222d5f3847bae7
MoyukhShabon <moyukh.work@gmail.com>
2026-01-12 18:27:33 +0800

    added script to get gatk-obmm results

---
+++ ffpe-snvf/get-gatk.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import polars as pl
+import os
+import glob
+from tqdm import tqdm
+
+def read_variants(path:str, columns: list = ["#CHROM", "POS", "REF", "ALT", "FILTER"]) -> pl.DataFrame:
+	variants = (
+		pl.read_csv(path, separator="\t", comment_prefix="##", infer_schema_length=1000, columns=columns)
+		.rename(lambda x: x.lstrip("#").lower())
+		.with_columns(pl.col("alt").str.split(","))
+		.explode("alt")
+	)
+	return variants
+
+## VCFs from each dataset to use for getting gatk obmm results
+dset_vcf = {
+	"PRJEB8754" : "vcf_filtered_pass-orient-pos-sb-ad",
+	"PRJEB44073" : "vcf_filtered_pass-orientation-dp10",
+	"SRP044740" : "vcf_filtered_pass-orientation-dp10",
+	"SRP065941" : "vcf_filtered_pass-orientation-dp10"
+}
+
+## Process each datasets
+for i, dset in enumerate(dset_vcf.keys()):
+	
+	var_set = dset_vcf[dset]
+	print(f"Processing Dataset: {dset} | {var_set}")
+
+	vcf_paths = glob.glob(f"../vcf/{dset}/{var_set}/*/*.vcf")
+
+	for path in tqdm(vcf_paths):
+		sample_name = path.split("/")[-2]
+
+		if "froz" in sample_name.lower():
+			continue
+
+		variants = read_variants(path)
+
+		outdir = f"{dset}/{var_set.removeprefix("vcf_")}/gatk-obmm/{sample_name}"
+		os.makedirs(outdir, exist_ok=True)
+
+		variants.write_csv(f"{outdir}/{sample_name}.gatk-obmm.tsv", separator="\t")
+
+
