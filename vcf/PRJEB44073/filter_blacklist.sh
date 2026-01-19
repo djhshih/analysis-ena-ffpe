5d00071ae1cf65a50bb5a829457dd6cf7ecb5a69
MoyukhShabon <moyukh.work@gmail.com>
2026-01-20 01:37:57 +0800

    added blacklist filtering script

---
+++ vcf/PRJEB44073/filter_blacklist.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euo pipefail
+
+root_outdir="vcf_filtered_pass-orientation-dp10-blacklist"
+mkdir -p $root_outdir
+
+blacklist_path="../../data/blacklists/master_blacklist.bed.gz"
+echo -e "Filtering using blacklist: $blacklist_path"
+
+for vcf in vcf_filtered_pass-orientation-dp10/*/*.vcf; do
+    
+    filename=$(basename $vcf)
+    sample_name=${filename%%.*}
+
+    echo $sample_name
+    
+    outdir="${root_outdir}/${sample_name}"
+    mkdir -p $outdir
+
+    echo -e "\nFiltering $filename"
+    bcftools view -T ^"$blacklist_path" $vcf -o "${outdir}/${sample_name}.vcf"
+    echo "Filtered VCF saved to ${outdir}/${sample_name}.vcf"
+
+    echo "Indexing ${outdir}/${sample_name}.vcf"
+    gatk IndexFeatureFile -I "${outdir}/${sample_name}.vcf"
+
+done
+
+echo -e "\nFinished."
