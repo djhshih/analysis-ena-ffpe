a1483e7a357876d979400af214af846908a3d801
MoyukhShabon <moyukh.work@gmail.com>
2026-02-12 20:50:46 +0800

    updated batch script creation script

--- ffpe-snvf/ffpe_artifact_filtering/create_scripts.py
+++ ffpe-snvf/ffpe_artifact_filtering/create_scripts.py
@@ -18,7 +18,7 @@ def create_filtering_scripts(models: list, vcf_path: str, bam_path: str, filtere
 			os.makedirs(outdir, exist_ok=True)
 					
 			## Template script for each model
-			template_dir = return_path_if_exists(f"{model}.R", abs=True)
+			template_dir = return_path_if_exists(f"../../common-ffpe-snvf/R/{model}.R", abs=True)
 			content = f"#!/usr/bin/env Rscript\nRscript {template_dir} --vcf '{vcf_path}' --ref '{ref_path}' --outdir '{filtered_outdir}'\n"
 			filename = f"{outdir}/{model}_{sample_name}.sh"
 			
@@ -43,9 +43,9 @@ def create_filtering_scripts(models: list, vcf_path: str, bam_path: str, filtere
 			outdir = "script"
 			os.makedirs(outdir, exist_ok=True)
 			
-			template_dir = return_path_if_exists(f"{model}.sh.template", abs=True)
+			template_dir = return_path_if_exists(f"../../common-ffpe-snvf/templates/ffpe-snvf/{model}.sh.template", abs=True)
 		
-			content = f"#!/bin/bash\nbash {template_dir} '{bam_path}' '{vcf_path}' '{filtered_outdir}' '{ref_path}'\n"
+			content = f"#!/bin/bash\nbash {template_dir} '{bam_path}' '{vcf_path}' '{filtered_outdir}/{model}/{sample_name}' '{ref_path}'\n"
 			filename = f"{outdir}/{model}_{sample_name}.sh"
 		
 			with open(filename, "w") as f:
@@ -62,8 +62,8 @@ ref_path = return_path_if_exists("../../data/ref/Homo_sapiens_assembly38.fasta",
 # Process each dataset
 
 ## PRJEB8754
-vcf_paths = sorted([os.path.abspath(path) for path in glob.glob("../../vcf/PRJEB8754/vcf_filtered_pass-orient-pos-sb-ad/*/*.vcf")])
-filtered_outdir = os.path.abspath("../PRJEB8754/filtered_pass-orient-pos-sb-ad")
+vcf_paths = sorted([os.path.abspath(path) for path in glob.glob("../../vcf/PRJEB8754/raw_filtermutectcalls_obmm_unfiltered_dup-unmarked/*/*.vcf")])
+filtered_outdir = os.path.abspath("../PRJEB8754/raw_filtermutectcalls_obmm_unfiltered_dup-unmarked")
 bam_dir = "../../data/PRJEB8754/bam_dup-unmarked"
 
 # skip non FFPE vcfs
@@ -71,20 +71,19 @@ for vcf_path in vcf_paths:
 	if "Frozen" in vcf_path:
 		continue
 	
-	vcf_filename = os.path.basename(vcf_path)
-	sample_name = vcf_filename.removesuffix(".vcf")
+	sample_name = os.path.basename(vcf_path).split(".")[0]
 	
 	bam_path = return_path_if_exists(f"{bam_dir}/{sample_name}/{sample_name}.bam", abs=True)
 	create_filtering_scripts(models, vcf_path, bam_path, filtered_outdir, sample_name, ref_path)
 
 
 ## PRJEB44073
-vcf_paths = sorted([os.path.abspath(path) for path in glob.glob("../../vcf/PRJEB44073/vcf_filtered_pass_orientation/*/*.vcf")])
-filtered_outdir = os.path.abspath("../PRJEB44073/filtered_pass_orientation")
+vcf_paths = sorted([os.path.abspath(path) for path in glob.glob("../../vcf/PRJEB44073/filtered_pass-orientation/*/*.vcf")])
+filtered_outdir = os.path.abspath("../PRJEB44073/filtered_pass-orientation")
 bam_dir = "../../data/PRJEB44073/bam"
 
-annot = pl.read_csv("../../annot/PRJEB44073/sample-info_stage2.tsv", separator = "\t")
-ffpe_samples = annot.filter(pl.col("preservation") == "FFPE").get_column("sample_alias").to_list()
+annot = pl.read_csv("../../annot/PRJEB44073/sample-info_stage3.tsv", separator = "\t")
+ffpe_samples = annot.filter(pl.col("preservation") == "FFPE").get_column("sample_name").to_list()
 
 # skip non FFPE vcfs
 vcf_paths = [path for path in vcf_paths if any(sample in path for sample in ffpe_samples)]
@@ -101,8 +100,8 @@ print("All filtering scripts created.")
 
 
 ## SRP044740
-vcf_paths = sorted([os.path.abspath(path) for path in glob.glob("../../vcf/SRP044740/vcf_filtered_pass_orientation/*/*.vcf")])
-filtered_outdir = os.path.abspath("../SRP044740/filtered_pass_orientation")
+vcf_paths = sorted([os.path.abspath(path) for path in glob.glob("../../vcf/SRP044740/filtered_pass-orientation/*/*.vcf")])
+filtered_outdir = os.path.abspath("../SRP044740/filtered_pass-orientation")
 bam_dir = "../../data/SRP044740/bam"
 
 # skip non FFPE vcfs
@@ -118,13 +117,13 @@ for vcf_path in vcf_paths:
 
 
 ## SRP065941
-vcf_paths = sorted([os.path.abspath(path) for path in glob.glob("../../vcf/SRP065941/vcf_filtered_pass_orientation/*/*.vcf")])
-filtered_outdir = os.path.abspath("../SRP065941/filtered_pass_orientation")
+vcf_paths = sorted([os.path.abspath(path) for path in glob.glob("../../vcf/SRP065941/filtered_pass-orientation/*/*.vcf")])
+filtered_outdir = os.path.abspath("../SRP065941/filtered_pass-orientation")
 bam_dir = "../../data/SRP065941/bam"
 
 # skip non FFPE vcfs
 for vcf_path in vcf_paths:
-	if "frozen" in vcf_path:
+	if "frozen" in vcf_path.lower():
 		continue
 	
 	vcf_filename = os.path.basename(vcf_path)
@@ -133,4 +132,3 @@ for vcf_path in vcf_paths:
 	bam_path = return_path_if_exists(f"{bam_dir}/{sample_name}/{sample_name}.bam", abs=True)
 	create_filtering_scripts(models, vcf_path, bam_path, filtered_outdir, sample_name, ref_path)
 
-	
